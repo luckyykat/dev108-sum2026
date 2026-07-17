84d48d8a396cd410a61063596e8c404444c46163
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-17 00:25:22 +0000

    asks if the customer wants to learn more

--- projects/project01-saleschat/main.py
+++ projects/project01-saleschat/main.py
@@ -12,3 +12,7 @@ print("Welcome to Happy Little Creations!")
 print("========================================")
 print("Hello there! My name is Bob, your happy little sales chatbot.")
 print()
+
+# Ask if the customer wants to learn more about the product.
+learn_more = input("Would you like to learn about our product? Enter yes or no: ")
+print()
