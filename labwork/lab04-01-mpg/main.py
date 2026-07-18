c4b874967752752af2f741ab9fe0b9a4322eb136
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-18 00:16:02 +0000

    added repeat_mpg added while statement

--- labwork/lab04-01-mpg/main.py
+++ labwork/lab04-01-mpg/main.py
@@ -6,20 +6,26 @@
 print("Welcome to The Miles Per Gallon application")
 print()
 
-# get input from the user
-miles_driven = float(input("Enter miles driven:         "))
-gallons_used = float(input("Enter gallons of gas used:  "))
+# set the loop to run atleast once
+repeat_mpg = "y"
 
-if miles_driven <= 0:
-    print("Miles driven must be greater than zero. Please try again.")
-elif gallons_used <= 0:
-    print("Gallons used must be greater than zero. Please try again.")
-else:
-    # inputs must be good
-    # calculate and display miles per gallon
-    mpg = round((miles_driven / gallons_used), 2)
-    print("Miles Per Gallon:          ", mpg)
+while repeat_mpg.lower() == "y":
 
-print()
+    # get input from the user
+    miles_driven = float(input("Enter miles driven:         "))
+    gallons_used = float(input("Enter gallons of gas used:  "))
+
+    if miles_driven <= 0:
+        print("Miles driven must be greater than zero. Please try again.")
+    elif gallons_used <= 0:
+        print("Gallons used must be greater than zero. Please try again.")
+    else:
+        # inputs must be good
+        # calculate and display miles per gallon
+        mpg = round((miles_driven / gallons_used), 2)
+        print("Miles Per Gallon:          ", mpg)
+
+    print()
+    repeat_mpg = input("Get entries for another trip? (y/n)")
 print("Bye")
 
