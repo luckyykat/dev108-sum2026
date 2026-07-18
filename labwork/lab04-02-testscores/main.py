6b60ff74055d0db3832038b492e72016bfaf8b6d
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-18 06:05:23 +0000

    added if and else statements

--- labwork/lab04-02-testscores/main.py
+++ labwork/lab04-02-testscores/main.py
@@ -3,7 +3,7 @@
 # Katherine Luciano
 
 # display a welcome message
-print("The Test Scores application")
+print("Welcome to The Test Scores Application")
 print()
 print("Enter test scores")
 print("Enter 'end' to end input")
@@ -15,14 +15,17 @@ score_total = 0
 test_score = 0
 
 while test_score != 999:
-    test_score = int(input("Enter test score: "))
-    if test_score >= 0 and test_score <= 100:
-        score_total += test_score
-        counter += 1
-    elif test_score == 999:
+    test_score = input("Enter test score: ")
+    if test_score.lower() == "end":
         break
     else:
-        print("Test score must be from 0 through 100. Score discarded. Try again.")
+        test_score = int(test_score)
+
+        if test_score >= 0 and test_score <= 100:
+            score_total += test_score
+            counter += 1
+        else:
+            print("Test score must be from 0 through 100. Please try again.")
 
 # calculate average score
 average_score = round(score_total / counter)
@@ -32,4 +35,4 @@ print("======================")
 print("Total Score:", score_total,
       "\nAverage Score:", average_score)
 print()
-print("Bye")
+print("Thank you for using the Test Scores Application, have a great day!")
