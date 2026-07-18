a207f475deccdff80ddc2293e421021828e312f5
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-18 20:32:42 +0000

    added outer loop

--- labwork/lab04-02-testscores/main.py
+++ labwork/lab04-02-testscores/main.py
@@ -5,34 +5,37 @@
 # display a welcome message
 print("Welcome to The Test Scores Application")
 print()
-print("Enter test scores")
-print("Enter 'end' to end input")
-print("======================")
+repeat_test_scores = "y"
+while repeat_test_scores.lower() == "y":
+    print("Enter test scores")
+    print("Enter 'end' to end input")
+    print("======================")
 
-# initialize variables
-counter = 0
-score_total = 0
-test_score = 0
+    # initialize variables
+    counter = 0
+    score_total = 0
+    test_score = 0
 
-while test_score != 999:
-    test_score = input("Enter test score: ")
-    if test_score.lower() == "end":
-        break
-    else:
-        test_score = int(test_score)
-
-        if test_score >= 0 and test_score <= 100:
-            score_total += test_score
-            counter += 1
+    while test_score != 999:
+        test_score = input("Enter test score: ")
+        if test_score.lower() == "end":
+            break
         else:
-            print("Test score must be from 0 through 100. Please try again.")
+            test_score = int(test_score)
 
-# calculate average score
-average_score = round(score_total / counter)
-                
-# format and display the result
-print("======================")
-print("Total Score:", score_total,
-      "\nAverage Score:", average_score)
-print()
+            if test_score >= 0 and test_score <= 100:
+                score_total += test_score
+                counter += 1
+            else:
+                print("Test score must be from 0 through 100. Please try again.")
+
+    # calculate average score
+    average_score = round(score_total / counter)
+                    
+    # format and display the result
+    print("======================")
+    print("Total Score:", score_total,
+        "\nAverage Score:", average_score)
+    print()
+    repeat_test_scores = input("Enter anoter set of test scores? y/n: ")
 print("Thank you for using the Test Scores Application, have a great day!")
