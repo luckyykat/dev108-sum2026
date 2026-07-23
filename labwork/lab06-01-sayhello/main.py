8f0c006316043341ffac056cb5648fbbc3bc1668
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-23 00:24:53 +0000

    started lab 06 and got the main done

--- labwork/lab06-01-sayhello/main.py
+++ labwork/lab06-01-sayhello/main.py
@@ -1,1 +1,18 @@
-# starting file for lab 6
+# DEV 108 - Lab Activity 6 - Say Hello
+# 07/22/26
+# Katherine Luciano
+
+
+# Import nameformat.py
+import nameformat
+
+# Main
+def main():
+    print("The NameFormat Module")
+    print("Hello!")
+
+# Ask for the names only once.
+    firstName = input("Please enter your first name: ")
+    lastName = input("Please enter your name: ")
+
+    
