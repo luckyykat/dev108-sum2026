a56e5765374da300137131f804dcd4a300421fb9
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-24 00:27:10 +0000

    added comments

--- labwork/lab06-01-sayhello/main.py
+++ labwork/lab06-01-sayhello/main.py
@@ -6,15 +6,16 @@
 # Import nameformat.py
 import nameformat
 
-# Main
+# Main, title and opening greeting
 def main():
-    print("The NameFormat Module")
+    print("Welcome to The NameFormat Module")
     print("Hello!")
 
-# Ask for the names only once.
+# Ask for the names
     firstName = input("Please enter your first name: ")
     lastName = input("Please enter your name: ")
 
+# Display the menu choices
     print("\n* * MENU * *")
     print("1 - Say Hello")
     print("2 - Output Full Name")
@@ -22,30 +23,40 @@ def main():
     print("4 - Read Documentation")
     print("5 - Exit")
 
+# Start with an empty choice to begin the loop 
     choice = ""
 
+#Keep repeating the menu options until the user selects 5
     while choice != "5":
+        # Ask the user to select an option
         choice = input("\nWhat is your choice? ")
 
+        # Option 1 calls the sayHello function
         if choice == "1":
             print(nameformat.sayHello(firstName))
 
+        # Option 2 calls the fullName function
         elif choice == "2":
             print(nameformat.fullName(firstName, lastName))
 
+        # Option 3 calls the lastNameFirst function
         elif choice == "3":
             print(nameformat.lastNameFirst(firstName, lastName))
 
+        # Option 4 displays the documentation for each function
         elif choice == "4":
             help(nameformat.sayHello)
             help(nameformat.fullName)
             help(nameformat.lastNameFirst)
 
+        # Option 5 displays a message before ending the loop
         elif choice == "5":
             print("Goodbye!")
 
+        # If the user enters anything other than 1 through 5
         else:
             print("Please choose an option from 1 through 5.")
 
+    # Run main when this file is executed directly
     if __name__ == "__main__":
         main()
