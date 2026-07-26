cc44ebe1f525c7b427ee5e4049527b8a805ca33b
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-26 01:01:53 +0000

    asked the user to play and redirected if they don't answer y/n

--- projects/project02-madlibs/main.py
+++ projects/project02-madlibs/main.py
@@ -6,3 +6,11 @@
 print("====================================")
 print("    MAD LIBS DRAG RACE EDITION")
 print("====================================")
+
+# Ask the user if they want to play 
+play_again= input("\nWould you like to play a game? (y/n): ").lower()
+
+# Redirect the user if they don't select y or n
+while play_again != "y" and play_again != "n":
+    print("Please enter y for yes or n for no.")
+    play_again = input("Would you like to play a game? (y/n): ").lower()
