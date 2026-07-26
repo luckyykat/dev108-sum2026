b6bc8b6947e4ff008d80f49c80d15b5034690705
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-26 00:43:15 +0000

    made title

--- projects/project02-madlibs/main.py
+++ projects/project02-madlibs/main.py
@@ -1,3 +1,8 @@
 # DEV 108 - Project 2 - Mad Libs
 # 07/25/26
 # Katherine Luciano 
+
+# Title
+print("====================================")
+print("    MAD LIBS DRAG RACE EDITION")
+print("====================================")
