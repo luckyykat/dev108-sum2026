7993dd39437fcc41bca5a6bb7930bf581e7e734d
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-26 06:04:42 +0000

    asked the user if they want to play redirect them if they say anything other than y/n added story count asked the user for thier name and greeting

--- projects/project02-madlibs/main.py
+++ projects/project02-madlibs/main.py
@@ -14,3 +14,11 @@ play_again= input("\nWould you like to play a game? (y/n): ").lower()
 while play_again != "y" and play_again != "n":
     print("Please enter y for yes or n for no.")
     play_again = input("Would you like to play a game? (y/n): ").lower()
+
+# Keeps track of how many stories the player completes
+story_count = 0
+
+# Ask the user for their name and greeting
+if play_again == "y":
+    player_name = input("\nFirst what is your name? ")
+    print("\nHello," + player_name + "! Welcome to Mad Libs Drag Race Edition.")
