71213e9a0110c69df620280f67253b5ee57e1f36
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-26 21:39:42 +0000

    fixed looping error was maissing input for story choice

--- projects/project02-madlibs/main.py
+++ projects/project02-madlibs/main.py
@@ -29,129 +29,134 @@ if play_again == "y":
         print(" a. The Makeover Challenege")
         print("b. The Girl Group Challenge")
 
-        story_choice =("\nWhich challenge would you like? (a/b): ").lower()
+        story_choice = input("\nWhich challenge would you like? (a/b): ").lower()
 
-    # Make Over Challenege Questions
-    if story_choice == "a":
-        print("\nThe Makeover Chalenege")
-        print("Create a new member of your drag family!\n")
+        while story_choice != "a" and story_choice != "b":
+            print("That challenge is not available. Please enter a or b.")
+            story_choice = input("Which challenge would you like? (a/b): ").lower()
+             
 
-        drag_name = input("1) What is your drag name? ")
-        partner_name = input("2) What is your makeover partner's regular name? ")
-        relationship = input("3) What is your relationship to this person? ")
-        drag_adjective = input(
-            "4) Enter a capitalized adjective for your partner's drag name: "
-            )
-        drag_noun = input("5) Enter a capitalized noun for your drag family name: ")
-        special_quality = input(
-            "6) Name a quality that makes them worthy of your House: "
-            )
-        outfit_color = input("7) Choose a color for your matching outfits: ")
-        outfit_material = input(
-            "8) Name an unusual material for the outfits: "
-            )
-        runway_move = input("9) Name a runway move or dance move: ")
+        # Make Over Challenege Questions
+        if story_choice == "a":
+            print("\nThe Makeover Chalenege")
+            print("Create a new member of your drag family!\n")
 
-        family_drag_name = drag_adjective + " " + drag_noun
-        house_name = drag_noun
+            drag_name = input("1) What is your drag name? ")
+            partner_name = input("2) What is your makeover partner's regular name? ")
+            relationship = input("3) What is your relationship to this person? ")
+            drag_adjective = input(
+                "4) Enter a capitalized adjective for your partner's drag name: "
+                )
+            drag_noun = input("5) Enter a capitalized noun for your drag family name: ")
+            special_quality = input(
+                "6) Name a quality that makes them worthy of your House: "
+                )
+            outfit_color = input("7) Choose a color for your matching outfits: ")
+            outfit_material = input(
+                "8) Name an unusual material for the outfits: "
+                )
+            runway_move = input("9) Name a runway move or dance move: ")
 
-    # Makeover Challenge Story
-        print("\n--------------------------------------------------------------")
-        print(player_name + ", here is your Makeover Challenge story:\n")
-        print(
-                drag_name + " invited their " + relationship + ", " + partner_name + ", "
-                "to compete in the Makeover Challenge."
-            )
-        print(
-                partner_name + " nervously entered the workroom and prepared "
-                "for a complete drag transformation."
-            )
-        print(
-                "They welcomed the new queen into the House of " + house_name
-                + " and gave them the family drag name " + family_drag_name + "."
-            )
-        print(
-                "Together, they created matching " + outfit_color + " outfits made "
-                "entirely from " + outfit_material + "."
-            )
-        print(
-                "They finished the runway with a dramatic " + runway_move + ", and "
-                "the judges cheered."
-            )
-        print(
-                "Because of their incredible " + special_quality + ", " + family_drag_name
-                + " proved they truly belonged in the House of " + house_name + "!"
-            )
+            family_drag_name = drag_adjective + " " + drag_noun
+            house_name = drag_noun
 
-# The Girl Group Challenge Questions
-elif story_choice == "b":
-        print("\n--- THE GIRL GROUP CHALLENGE ---")
-        print("Get ready to write and perform a brand-new drag anthem!\n")
+        # Makeover Challenge Story
+            print("\n--------------------------------------------------------------")
+            print(player_name + ", here is your Makeover Challenge story:\n")
+            print(
+                    drag_name + " invited their " + relationship + ", " + partner_name + ", "
+                    "to compete in the Makeover Challenge."
+                )
+            print(
+                    partner_name + " nervously entered the workroom and prepared "
+                    "for a complete drag transformation."
+                )
+            print(
+                    "They welcomed the new queen into the House of " + house_name
+                    + " and gave them the family drag name " + family_drag_name + "."
+                )
+            print(
+                    "Together, they created matching " + outfit_color + " outfits made "
+                    "entirely from " + outfit_material + "."
+                )
+            print(
+                    "They finished the runway with a dramatic " + runway_move + ", and "
+                    "the judges cheered."
+                )
+            print(
+                    "Because of their incredible " + special_quality + ", " + family_drag_name
+                    + " proved they truly belonged in the House of " + house_name + "!"
+                )
 
-        group_name = input("1) Give your girl group a name: ")
-        music_genre = input("2) Name a type of music: ")
-        song_title = input("3) Give the group's song a title: ")
-        song_topic = input("4) What unusual topic is the song about? ")
-        silly_lyric = input("5) Enter a silly lyric or catchphrase: ")
-        dance_move = input("6) Name a signature dance move: ")
-        stage_prop = input("7) Name an unusual object to use as a prop: ")
+    # The Girl Group Challenge Questions
+        elif story_choice == "b":
+            print("\n--- THE GIRL GROUP CHALLENGE ---")
+            print("Get ready to write and perform a brand-new drag anthem!\n")
 
-        # Makes sure the user eneters a number 1-10
-        dancer_count = int(input(
-            "8) Choose a number of backup dancers from 1 to 10: "
-        ))
+            group_name = input("1) Give your girl group a name: ")
+            music_genre = input("2) Name a type of music: ")
+            song_title = input("3) Give the group's song a title: ")
+            song_topic = input("4) What unusual topic is the song about? ")
+            silly_lyric = input("5) Enter a silly lyric or catchphrase: ")
+            dance_move = input("6) Name a signature dance move: ")
+            stage_prop = input("7) Name an unusual object to use as a prop: ")
 
-        while dancer_count < 1 or dancer_count > 10:
-            print("Please enter a whole number from 1 to 10.")
+            # Makes sure the user eneters a number 1-10
             dancer_count = int(input(
                 "8) Choose a number of backup dancers from 1 to 10: "
             ))
 
-        rupaul_emotion = input(
-            "9) Enter an emotion describing RuPaul's reaction: "
-        )
+            while dancer_count < 1 or dancer_count > 10:
+                print("Please enter a whole number from 1 to 10.")
+                dancer_count = int(input(
+                    "8) Choose a number of backup dancers from 1 to 10: "
+                ))
 
-        if dancer_count == 1:
-            dancer_phrase = "one fearless backup dancer"
-        elif dancer_count <= 5:
-            dancer_phrase = str(dancer_count) + " energetic backup dancers"
-        else:
-            dancer_phrase = "a huge crew of " + str(dancer_count) + " backup dancers"
+            rupaul_emotion = input(
+                "9) Enter an emotion describing RuPaul's reaction: "
+            )
 
-# Girl group Challenge Story
-print("\n--------------------------------------------------------------")
-print(player_name + ", here is your Girl Group Challenge story:\n")
-print(
-                "The contestant joined the newest drag sensation, " + group_name + "."
-            )
-print(
-                "Together, the group recorded a " + music_genre + " anthem titled "
-                "\"" + song_title + ".\""
-            )
-print(
-                "The song was about " + song_topic + ", and its unforgettable lyric "
-                "was, \"" + silly_lyric + "!\""
-            )
-print(
-                "On the main stage, the group performed the " + dance_move + " with "
-                + dancer_phrase + " while waving a " + stage_prop + "."
-            )
-print(
-                "RuPaul looked " + rupaul_emotion + " and declared the performance "
-                "a drag masterpiece."
-            )
-print(group_name + " had officially become the next big girl group!")
+            if dancer_count == 1:
+                dancer_phrase = "one fearless backup dancer"
+            elif dancer_count <= 5:
+                dancer_phrase = str(dancer_count) + " energetic backup dancers"
+            else:
+                dancer_phrase = "a huge crew of " + str(dancer_count) + " backup dancers"
 
-story_count += 1
-print("--------------------------------------------------------------")
+    # Girl group Challenge Story
+    print("\n--------------------------------------------------------------")
+    print(player_name + ", here is your Girl Group Challenge story:\n")
+    print(
+                    "The contestant joined the newest drag sensation, " + group_name + "."
+                )
+    print(
+                    "Together, the group recorded a " + music_genre + " anthem titled "
+                    "\"" + song_title + ".\""
+                )
+    print(
+                    "The song was about " + song_topic + ", and its unforgettable lyric "
+                    "was, \"" + silly_lyric + "!\""
+                )
+    print(
+                    "On the main stage, the group performed the " + dance_move + " with "
+                    + dancer_phrase + " while waving a " + stage_prop + "."
+                )
+    print(
+                    "RuPaul looked " + rupaul_emotion + " and declared the performance "
+                    "a drag masterpiece."
+                )
+    print(group_name + " had officially become the next big girl group!")
 
-# Keeps track of how many stories created
-if story_count == 1:
-        print("\nYou have created 1 story.")
-else:
-        print("\nYou have created", story_count, "stories.")
+    story_count += 1
+    print("--------------------------------------------------------------")
 
-play_again = input("\nWould you like to play again? (y/n): ").lower()
+    # Keeps track of how many stories created
+    if story_count == 1:
+            print("\nYou have created 1 story.")
+    else:
+            print("\nYou have created", story_count, "stories.")
+
+    play_again = input("\nWould you like to play again? (y/n): ").lower()
 
     # Ask the user if they want to replay and farewell
 while play_again != "y" and play_again != "n":
