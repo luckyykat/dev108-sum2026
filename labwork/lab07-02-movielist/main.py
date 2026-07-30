f425f307108ba19f3d0304aa53faa583b7f6d87a
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-30 06:16:07 +0000

    fixed missing quotation marks on line 34 added spaces in message "was released in" added new function to find by year

--- labwork/lab07-02-movielist/main.py
+++ labwork/lab07-02-movielist/main.py
@@ -13,14 +13,7 @@ def display_menu():
     print("find - Find movies by a year")
     print("exit - Exit program")
     print()
-
-def main():
-    # Organizes each movie by name, year and price
-    movie_list = [
-        ["Monty Python and the Holy Grail", 1975, 9.95],
-        ["On the Waterfront", 1954, 5.59],
-        ["Cat on a Hot Tin Roof", 1958, 7.95]
-    ]      
+    
 
 def list(movie_list):
     if len(movie_list) == 0:
@@ -38,7 +31,7 @@ def list(movie_list):
 def add(movie_list):
     name = input("Name: ")
     year = int(input("Year: "))
-    price = float(input(Price: ))
+    price = float(input("Price: "))
 
     # Stores the name, year, and price in a new movie list
     movie = []
@@ -63,11 +56,19 @@ def delete(movie_list):
 # Finds and dispays movies released in a specific year
 def find_by_year(movie_list):
     year = int(input("Year: "))
+
+    for movie in movie_list:
+        if movie[1] == year:
+            print(movie[0] + " was released in " + str(year) + ".")
+        print()
         
 def main():
-    movie_list = [["Monty Python and the Holy Grail", 1975],
-                  ["On the Waterfront", 1954],
-                  ["Cat on a Hot Tin Roof", 1958]]
+    # Organizes each movie by name, year and price
+    movie_list = [
+        ["Monty Python and the Holy Grail", 1975, 9.95],
+        ["On the Waterfront", 1954, 5.59],
+        ["Cat on a Hot Tin Roof", 1958, 7.95]
+    ]
     
     display_menu()
     while True:        
@@ -76,6 +77,8 @@ def main():
             list(movie_list)
         elif command == "add":
             add(movie_list)
+        elif command == "find":
+            find_by_year(movie_list)
         elif command == "del":
             delete(movie_list)
         elif command == "exit":
