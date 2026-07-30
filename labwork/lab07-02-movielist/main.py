d86c51a2798837ffd492bf8a616b7bd6a88beccb
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-07-30 04:35:15 +0000

    fixed quation marks typo with monthy python movie title

--- labwork/lab07-02-movielist/main.py
+++ labwork/lab07-02-movielist/main.py
@@ -17,7 +17,7 @@ def display_menu():
 def main():
     # Organizes each movie by name, year and price
     movie_list = [
-        ["Monty Python and the Holy Grail, 1975, 9.95"],
+        ["Monty Python and the Holy Grail", 1975, 9.95],
         ["On the Waterfront", 1954, 5.59],
         ["Cat on a Hot Tin Roof", 1958, 7.95]
     ]      
