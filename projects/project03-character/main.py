b43f2b5190a603ee9800c3204e800df409306299
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-08-02 02:12:53 +0000

    started project 3 added random generator displayed welcome message

--- projects/project03-character/main.py
+++ projects/project03-character/main.py
@@ -1,1 +1,23 @@
-# Placeholder for project 3 character generator program
+# DEV 108 - Project 3 - Character Gnerator
+# 08/01/26
+# Katherine Luciano
+
+# Generates random numbers 
+import random 
+
+# Display welcome message
+def display_welcome(): 
+    print("********************************")
+    print("   Sanrio Character Generator")
+    print("********************************")
+    print()
+
+# Character generator
+def main():
+    display_welcome()
+
+    create_character = input(
+        "Would you like to generate a Sanrio character? yes or no: "
+    )
+if __name__ == "__main__":
+    main()
