d8dbb215767917fcc86d6d438c5430fc2e866954
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-08-09 03:58:38 +0000

    fixed line 13

--- labwork/lab09-01-invoice/main.py
+++ labwork/lab09-01-invoice/main.py
@@ -10,7 +10,7 @@ print("The Invoice program")
 print()
 
 # locale for US currency formatting
-lc.setlocale(lc.LC_ALL, "")
+lc.setlocale(lc.LC_ALL, "en_US.UTF-8")
 
 choice = "y"
 while choice == "y":
