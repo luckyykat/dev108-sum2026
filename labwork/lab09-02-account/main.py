ab06540c872bcea00a9bab67ddc3759aedb13201
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-08-09 23:21:38 +0000

    added comments to code

--- labwork/lab09-02-account/main.py
+++ labwork/lab09-02-account/main.py
@@ -7,6 +7,7 @@ def main():
     print("Welcome to the Account Validaton Program")
     print()
 
+    # Get user information
     full_name = get_full_name()
     print()
     
@@ -19,13 +20,15 @@ def main():
     phone = get_phone()
     print()
 
+    # Get user first name and show confirmation message
     first_name = get_first_name(full_name)
     print(f"Hello {first_name}, thank you for creating an accouunt.")
     print(f"We'll text your confirmation code to this number: {phone}")
 
     # Exit message
     print("Thank you for using the Account Validation Program, take care!")             
-    
+
+# Loop for user to provide full name     
 def get_full_name():
     while True:
         name = input("Enter full name:       ").strip()
@@ -33,12 +36,14 @@ def get_full_name():
             return name
         else:
             print("You must enter your full name.")
-    
+
+# Finds the space and slices the name     
 def get_first_name(full_name):
     index1 = full_name.find(" ")
     first_name = full_name[:index1]
     return first_name
-    
+
+# Loop until users passwords meets the requirements    
 def get_password():
     while True:
         digit = False
@@ -55,6 +60,7 @@ def get_password():
         else:
             return password
 
+# Loop until user contains "@" and ends with ".com"
 def get_email():
     while True:
         email = input("Enter email address:    ").strip().lower()
@@ -63,9 +69,12 @@ def get_email():
         else:
             print("Please enter a 10 digit phone number.")
 
+# Loop until user enters 10-digit phone number
 def get_phone():
     while True:
         phone = input("Enter phone number:    ").strip()
+
+
 
         
 if __name__ == "__main__":
