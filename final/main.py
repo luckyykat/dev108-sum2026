e426285db24289152839eaca18c27eefaaf0572d
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2026-08-13 01:24:33 +0000

    added function that saves new user accounts

--- final/main.py
+++ final/main.py
@@ -114,3 +114,42 @@ def make_note():
     note = random.choice(notes)
 
     return note
+
+# This function creates and saves a new user account
+def create_account():
+    humans = load_humans()
+
+    print("\nCreate a new Mars Base HUman Account")
+    print("----------------------------------------")
+
+    first_name = get_text("First name: ")
+    last_name = get_text("Last name: ")
+    age = get_age()
+
+    human_id = make_id(humans)
+    email = make_email(first_name,  last_name, human_id)
+    password = make_password()
+    pod = make_pod()
+    note = make_note()
+
+    new_human = []
+    new_human.append(human_id)
+    new_human.append(first_name)
+    new_human.append(last_name)
+    new_human.append(age)
+    new_human.append(email)
+    new_human.append(password)
+    new_human.append(pod)
+    new_human.append(note)
+
+    humans.append(new_human)
+    save_humans(humans)
+
+    print("\nNew Mars Base HUman Account Created")
+    print("---------------------------------------")
+    print("ID: {}".format(first_name, last_name))
+    print("Name: {} {}".format(age))
+    print("Email: {}".format(email))
+    print("Password: {}".format(password))
+    print("Living Pod: {}".format(pod))
+    print("Note: {}".format(note))
